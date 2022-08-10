c7364e028c478e714df0d8d629ff6214b013c345
Michael Hueschen <m@mhueschen.space>
2022-08-10 08:36:36 -0600

    activateApp method wrap attempt
    this approach doesn't really work because the `activate_app_js` method
    only exists as a method on the opaque type `AdminWebsocketJs`, for which
    we do not have a constructor (and for which we cannot really make a
    constructor, b/c `connect` is the constructor and is async / returns a
    Promise).

--- src/holochain_client_wrapper.rs
+++ src/holochain_client_wrapper.rs
@@ -13,13 +13,25 @@ impl From<AdminWebsocket> for JsValue {
 
 #[wasm_bindgen(module = "/src/holochain_client_wrapper.js")]
 extern "C" {
+    type AdminWebsocketJs;
+
     #[wasm_bindgen(catch, js_namespace = AdminWebsocket, js_name="connect")]
     async fn connect_js(url: String, timeout: Option<u32>) -> Result<JsValue, JsValue>;
+
+    #[wasm_bindgen(method, catch, js_namespace = AdminWebsocket, js_name="activateApp")]
+    async fn activate_app_js(this: &AdminWebsocketJs, installed_app_id: String) -> Result<JsValue, JsValue>;
 }
 
 pub async fn connect(url: String, timeout: Option<u32>) -> Result<AdminWebsocket, String> {
     match connect_js(url, timeout).await {
         Ok(js_ws) => Ok(AdminWebsocket { js_ws }),
+        Err(js_err) => Err(format!("{:?}", js_err)),
+    }
+}
+
+pub async fn activate_app(installed_app_id: String) -> Result<(), String> {
+    match activate_app_js(installed_app_id).await {
+        Ok(_null) => Ok(()),
         Err(js_err) => Err(format!("{:?}", js_err)),
     }
 }
