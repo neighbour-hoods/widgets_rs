ef269eecb5ac8feffbbe7075ef2f1bd8c78c31f5
Michael Hueschen <m@mhueschen.space>
2022-08-11 11:36:11 -0600

    migrate frontend code into frontend/ crate

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,7 +1,7 @@
 require('esbuild').build({
   entryPoints: ['./submodules/holochain-client-js/src/index.ts'],
   bundle: true,
-  outfile: 'src/holochain_client_wrapper.js',
+  outfile: 'crates/frontend/src/holochain_client_wrapper.js',
   format: 'esm',
 }).then(_ => console.log("success 🚀"))
   .catch(() => process.exit(1))
