2dd7aa46b3a7000a4834a6ae770d937e77d87ac2
Michael Hueschen <m@mhueschen.space>
2022-08-11 13:07:17 -0600

    switch `generate_call` to an attribute proc macro
    prior approach didn't work - needed access to the ItemEnum (afaict it's
    not possible to "get it by reference", and I think this way will work,
    so we'll just roll with it for now).
    
    this compiles and replicates the prior behavior of `call` being
    callable, but then crashing on the `todo!` 👌

--- crates/frontend/src/holochain_client_wrapper.rs
+++ crates/frontend/src/holochain_client_wrapper.rs
@@ -15,35 +15,10 @@ impl From<AdminWebsocket> for JsValue {
     }
 }
 
+#[generate_call]
 pub enum AdminWsCmd {
     EnableApp { installed_app_id: String },
     DisableApp { installed_app_id: String },
-}
-
-impl AdminWebsocket {
-    generate_call!{AdminWsCmd}
-
-    pub async fn disable_app(&self, installed_app_id: String) -> Result<(), JsValue> {
-        let tag: &str = "disableApp";
-        let method: Function = Reflect::get(&self.js_ws, &JsValue::from_str(&tag))?.dyn_into()?;
-        let payload: JsValue =
-            mk_tagged_obj(tag, stringify!(installed_app_id), installed_app_id.into())?;
-        let promise: Promise = method.call1(&self.js_ws, &payload)?.dyn_into()?;
-        let future: JsFuture = promise.into();
-        future.await?;
-        Ok(())
-    }
-
-    pub async fn enable_app(&self, installed_app_id: String) -> Result<(), JsValue> {
-        let tag: &str = "enableApp";
-        let method: Function = Reflect::get(&self.js_ws, &JsValue::from_str(&tag))?.dyn_into()?;
-        let payload: JsValue =
-            mk_tagged_obj(tag, stringify!(installed_app_id), installed_app_id.into())?;
-        let promise: Promise = method.call1(&self.js_ws, &payload)?.dyn_into()?;
-        let future: JsFuture = promise.into();
-        future.await?;
-        Ok(())
-    }
 }
 
 /// `tag` is more like "method name" here.
