1980ebb8086a6ebea592d8a191f649bce9745723
Michael Hueschen <m@mhueschen.space>
2022-08-12 17:48:05 -0600

    AppWebsocket first draft
    largely borrow for AdminWebsocket. parameterize `generate_call` macro,
    via attributes, so it can be used for AppWebsocket too.
    
    support / make-testable AppWebsocket stuff in the Yew frontend.
    test `connect` and `appInfo` methods - they work.

--- crates/frontend/src/app.rs
+++ crates/frontend/src/app.rs
@@ -4,7 +4,10 @@ use weblog::{console_error, console_log};
 use yew::{html::Scope, prelude::*};
 
 use crate::{
-    holochain_client_wrapper::{connect, AdminWebsocket, AdminWsCmd, AdminWsCmdResponse},
+    holochain_client_wrapper::{
+        connect_admin_ws, connect_app_ws, AdminWebsocket, AdminWsCmd, AdminWsCmdResponse,
+        AppWebsocket, AppWsCmd, AppWsCmdResponse,
+    },
     myclass::MyClass,
 };
 
@@ -12,22 +15,29 @@ pub enum Msg {
     AddOne,
     SubOne,
     SetNumber(u32),
+    // AdminWs
     AdminWsConnected(AdminWebsocket),
     AdminWsError(String),
     AdminWsCmd(AdminWsCmd),
     AdminWsCmdResponse(Result<AdminWsCmdResponse, JsValue>),
+    // AppWs
+    AppWsConnected(AppWebsocket),
+    AppWsError(String),
+    AppWsCmd(AppWsCmd),
+    AppWsCmdResponse(Result<AppWsCmdResponse, JsValue>),
 }
 
 #[derive(Clone, Debug)]
-pub enum AdminWsState {
-    Present(AdminWebsocket),
+pub enum WsState<WS> {
+    Present(WS),
     Absent(String),
 }
 
 pub struct Model {
     value: i64,
     myclass: MyClass,
-    admin_ws: AdminWsState,
+    admin_ws: WsState<AdminWebsocket>,
+    app_ws: WsState<AppWebsocket>,
 }
 
 impl Component for Model {
@@ -36,15 +46,22 @@ impl Component for Model {
 
     fn create(ctx: &Context<Self>) -> Self {
         ctx.link().send_future(async {
-            match connect("ws://localhost:9000".into(), None).await {
+            match connect_admin_ws("ws://localhost:9000".into(), None).await {
                 Ok(ws) => Msg::AdminWsConnected(ws),
                 Err(err) => Msg::AdminWsError(err),
+            }
+        });
+        ctx.link().send_future(async {
+            match connect_app_ws("ws://localhost:9999".into(), None).await {
+                Ok(ws) => Msg::AppWsConnected(ws),
+                Err(err) => Msg::AppWsError(err),
             }
         });
         Self {
             value: 0,
             myclass: MyClass::new(),
-            admin_ws: AdminWsState::Absent("".into()),
+            admin_ws: WsState::Absent("".into()),
+            app_ws: WsState::Absent("".into()),
         }
     }
 
@@ -67,20 +84,22 @@ impl Component for Model {
                 true
             }
             Msg::AdminWsConnected(ws) => {
-                self.admin_ws = AdminWsState::Present(ws.clone());
+                self.admin_ws = WsState::Present(ws.clone());
                 console_log!("Holochain admin ws connected: {:?}", ws);
                 true
             }
             Msg::AdminWsError(err) => {
-                self.admin_ws = AdminWsState::Absent(err.clone());
-                console_error!(err);
+                self.admin_ws = WsState::Absent(err.clone());
+                console_error!(format!("AdminWsError: {}", err));
                 true
             }
             Msg::AdminWsCmd(cmd) => {
                 let ws_clone = self.admin_ws.clone();
                 match ws_clone {
-                    AdminWsState::Absent(_err) => console_log!("AdminWsCmd but no admin ws"),
-                    AdminWsState::Present(ws) => {
+                    WsState::Absent(err) => {
+                        console_error!(format!("AdminWsCmd - WsState::Absent: {}", err))
+                    }
+                    WsState::Present(ws) => {
                         console_log!("AdminWsCmd w/ admin ws");
                         ctx.link().send_future(async move {
                             Msg::AdminWsCmdResponse(ws.call(cmd).await)
@@ -100,13 +119,51 @@ impl Component for Model {
                 };
                 false
             }
+
+            // AppWs
+            // TODO consider consolidating / deduplicating admin and app bits
+            Msg::AppWsConnected(ws) => {
+                self.app_ws = WsState::Present(ws.clone());
+                console_log!("Holochain app ws connected: {:?}", ws);
+                true
+            }
+            Msg::AppWsError(err) => {
+                self.app_ws = WsState::Absent(err.clone());
+                console_error!(format!("AppWsError: {}", err));
+                true
+            }
+            Msg::AppWsCmd(cmd) => {
+                let ws_clone = self.app_ws.clone();
+                match ws_clone {
+                    WsState::Absent(err) => {
+                        console_error!(format!("AppWsCmd - WsState::Absent: {}", err))
+                    }
+                    WsState::Present(ws) => {
+                        console_log!("AppWsCmd w/ app ws");
+                        ctx.link()
+                            .send_future(async move { Msg::AppWsCmdResponse(ws.call(cmd).await) });
+                    }
+                };
+                false
+            }
+            Msg::AppWsCmdResponse(resp) => {
+                match resp {
+                    Ok(val) => {
+                        console_log!(format!("AppWsCmdResponse: {:?}", val));
+                    }
+                    Err(err) => {
+                        console_error!(format!("AppWsCmdResponse: error: {:?}", err));
+                    }
+                };
+                false
+            }
         }
     }
 
     fn view(&self, ctx: &Context<Self>) -> Html {
         let ws_debug: String = match &self.admin_ws {
-            AdminWsState::Absent(_err) => "admin_ws absent".into(),
-            AdminWsState::Present(ws) => format!("typeof: {:?}", ws.js_ws.js_typeof(),),
+            WsState::Absent(_err) => "admin_ws absent".into(),
+            WsState::Present(ws) => format!("typeof: {:?}", ws.js_ws.js_typeof(),),
         };
         let enable_app_handler = |input: String| {
             Ok(Msg::AdminWsCmd(AdminWsCmd::EnableApp {
@@ -135,6 +192,11 @@ impl Component for Model {
                 <button onclick={ctx.link().callback(move |_| Msg::AdminWsCmd(msg.clone()))}>{ format!("{:?}", msg_) }</button>
             }
         };
+        let app_info_handler = |input: String| {
+            Ok(Msg::AppWsCmd(AppWsCmd::AppInfo {
+                installed_app_id: input,
+            }))
+        };
         html! {
             <div>
                 <button onclick={ctx.link().callback(|_| Msg::AddOne)}>{ "+1" }</button>
@@ -144,7 +206,7 @@ impl Component for Model {
                 <button onclick={ctx.link().callback(|_| Msg::SetNumber(0))}>{ "set number" }</button>
                 <p>{self.myclass.render()}</p>
 
-                <p>{format!("{:?}", self.admin_ws)}</p>
+                <p>{format!("admin_ws: {:?}", self.admin_ws)}</p>
                 <p>{format!("{:?}", ws_debug)}</p>
 
                 { self.view_string_input(ctx.link(), enable_app_handler, "enable_app".into(), "enable which app?".into()) }
@@ -162,6 +224,12 @@ impl Component for Model {
                 { mk_nullary_button(AdminWsCmd::ListCellIds) }
                 <br/>
                 { mk_nullary_button(AdminWsCmd::ListActiveApps) }
+
+                <br/>
+                <br/>
+                <p>{format!("app_ws: {:?}", self.app_ws)}</p>
+                <br/>
+                { self.view_string_input(ctx.link(), app_info_handler, "app_info".into(), "app info for which app?".into()) }
             </div>
         }
     }
--- crates/frontend/src/holochain_client_wrapper.rs
+++ crates/frontend/src/holochain_client_wrapper.rs
@@ -4,6 +4,19 @@ use wasm_bindgen_futures::JsFuture;
 
 use macros::generate_call;
 
+#[wasm_bindgen(module = "/src/holochain_client_wrapper.js")]
+extern "C" {
+    #[wasm_bindgen(catch, js_namespace = AdminWebsocket, js_name="connect")]
+    async fn connect_admin_ws_js(url: String, timeout: Option<u32>) -> Result<JsValue, JsValue>;
+
+    #[wasm_bindgen(catch, js_namespace = AppWebsocket, js_name="connect")]
+    async fn connect_app_ws_js(url: String, timeout: Option<u32>) -> Result<JsValue, JsValue>;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// AdminWebsocket
+////////////////////////////////////////////////////////////////////////////////
+
 #[derive(Clone, Debug)]
 pub struct AdminWebsocket {
     pub js_ws: JsValue,
@@ -15,7 +28,19 @@ impl From<AdminWebsocket> for JsValue {
     }
 }
 
-#[generate_call]
+pub async fn connect_admin_ws(url: String, timeout: Option<u32>) -> Result<AdminWebsocket, String> {
+    match connect_admin_ws_js(url, timeout).await {
+        Ok(js_ws) => Ok(AdminWebsocket { js_ws }),
+        Err(js_err) => Err(format!("{:?}", js_err)),
+    }
+}
+
+#[generate_call(
+    AdminWebsocket,
+    AdminWsCmd,
+    AdminWsCmdResponse,
+    parse_admin_ws_cmd_response
+)]
 #[derive(Clone, Debug)]
 pub enum AdminWsCmd {
     EnableApp { installed_app_id: String },
@@ -57,15 +82,47 @@ fn parse_admin_ws_cmd_response(val: JsValue, tag: String) -> AdminWsCmdResponse 
     }
 }
 
-#[wasm_bindgen(module = "/src/holochain_client_wrapper.js")]
-extern "C" {
-    #[wasm_bindgen(catch, js_namespace = AdminWebsocket, js_name="connect")]
-    async fn connect_js(url: String, timeout: Option<u32>) -> Result<JsValue, JsValue>;
+////////////////////////////////////////////////////////////////////////////////
+// AppWebsocket
+////////////////////////////////////////////////////////////////////////////////
+
+#[derive(Clone, Debug)]
+pub struct AppWebsocket {
+    pub js_ws: JsValue,
 }
 
-pub async fn connect(url: String, timeout: Option<u32>) -> Result<AdminWebsocket, String> {
-    match connect_js(url, timeout).await {
-        Ok(js_ws) => Ok(AdminWebsocket { js_ws }),
+impl From<AppWebsocket> for JsValue {
+    fn from(ws: AppWebsocket) -> Self {
+        ws.js_ws
+    }
+}
+
+pub async fn connect_app_ws(url: String, timeout: Option<u32>) -> Result<AppWebsocket, String> {
+    match connect_app_ws_js(url, timeout).await {
+        Ok(js_ws) => Ok(AppWebsocket { js_ws }),
         Err(js_err) => Err(format!("{:?}", js_err)),
+    }
+}
+
+#[generate_call(AppWebsocket, AppWsCmd, AppWsCmdResponse, parse_app_ws_cmd_response)]
+#[derive(Clone, Debug)]
+pub enum AppWsCmd {
+    AppInfo { installed_app_id: String },
+    // CallZome { cell_id, zome_name, fn_name, payload, provenance, cap }
+}
+
+#[derive(Clone, Debug)]
+pub enum AppWsCmdResponse {
+    AppInfo(JsValue),
+    // CallZome(JsValue),
+}
+
+fn parse_app_ws_cmd_response(val: JsValue, tag: String) -> AppWsCmdResponse {
+    match tag.as_str() {
+        "AppInfo" => AppWsCmdResponse::AppInfo(val),
+        other => panic!(
+            "parse_app_ws_cmd_response: impossible: received unknown tag: {}",
+            other
+        ),
     }
 }
