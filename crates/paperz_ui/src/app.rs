c4aab89a7dbc5b8ffb069a36b597e214cc8c3bff
Michael Hueschen <m@mhueschen.space>
2022-08-15 19:34:00 -0600

    extract / consolidate common Yew Msg elements from UIs
    App/AdminWebsocket Msg enum variants.
    
    slightly clunk, but seems nicer to share these things.
    
    harder to abstract other aspects tho.

--- crates/paperz_ui/src/app.rs
+++ crates/paperz_ui/src/app.rs
@@ -1,4 +1,3 @@
-use wasm_bindgen::prelude::*;
 use web_sys::HtmlInputElement as InputElement;
 use weblog::{console_error, console_log};
 use yew::{html::Scope, prelude::*};
@@ -7,24 +6,11 @@ use holochain_client_wrapper::{
     connect_admin_ws, connect_app_ws, AdminWebsocket, AdminWsCmd, AdminWsCmdResponse, AppWebsocket,
     AppWsCmd, AppWsCmdResponse,
 };
+use widget_helpers::{WsMsg, WsState};
 
 pub enum Msg {
-    // AdminWs
-    AdminWsConnected(AdminWebsocket),
-    AdminWsError(String),
-    AdminWsCmd(AdminWsCmd),
-    AdminWsCmdResponse(Result<AdminWsCmdResponse, JsValue>),
-    // AppWs
-    AppWsConnected(AppWebsocket),
-    AppWsError(String),
-    AppWsCmd(AppWsCmd),
-    AppWsCmdResponse(Result<AppWsCmdResponse, JsValue>),
-}
-
-#[derive(Clone, Debug)]
-pub enum WsState<WS> {
-    Present(WS),
-    Absent(String),
+    AdminWs(WsMsg<AdminWebsocket, AdminWsCmd, AdminWsCmdResponse>),
+    AppWs(WsMsg<AppWebsocket, AppWsCmd, AppWsCmdResponse>),
 }
 
 pub struct Model {
@@ -39,14 +25,14 @@ impl Component for Model {
     fn create(ctx: &Context<Self>) -> Self {
         ctx.link().send_future(async {
             match connect_admin_ws("ws://localhost:9000".into(), None).await {
-                Ok(ws) => Msg::AdminWsConnected(ws),
-                Err(err) => Msg::AdminWsError(err),
+                Ok(ws) => Msg::AdminWs(WsMsg::Connected(ws)),
+                Err(err) => Msg::AdminWs(WsMsg::Error(err)),
             }
         });
         ctx.link().send_future(async {
             match connect_app_ws("ws://localhost:9999".into(), None).await {
-                Ok(ws) => Msg::AppWsConnected(ws),
-                Err(err) => Msg::AppWsError(err),
+                Ok(ws) => Msg::AppWs(WsMsg::Connected(ws)),
+                Err(err) => Msg::AppWs(WsMsg::Error(err)),
             }
         });
         Self {
@@ -57,17 +43,17 @@ impl Component for Model {
 
     fn update(&mut self, ctx: &Context<Self>, msg: Self::Message) -> bool {
         match msg {
-            Msg::AdminWsConnected(ws) => {
+            Msg::AdminWs(WsMsg::Connected(ws)) => {
                 self.admin_ws = WsState::Present(ws.clone());
                 console_log!("Holochain admin ws connected: {:?}", ws);
                 true
             }
-            Msg::AdminWsError(err) => {
+            Msg::AdminWs(WsMsg::Error(err)) => {
                 self.admin_ws = WsState::Absent(err.clone());
                 console_error!(format!("AdminWsError: {}", err));
                 true
             }
-            Msg::AdminWsCmd(cmd) => {
+            Msg::AdminWs(WsMsg::Cmd(cmd)) => {
                 let ws_clone = self.admin_ws.clone();
                 match ws_clone {
                     WsState::Absent(err) => {
@@ -76,13 +62,13 @@ impl Component for Model {
                     WsState::Present(ws) => {
                         console_log!("AdminWsCmd w/ admin ws");
                         ctx.link().send_future(async move {
-                            Msg::AdminWsCmdResponse(ws.call(cmd).await)
+                            Msg::AdminWs(WsMsg::CmdResponse(ws.call(cmd).await))
                         });
                     }
                 };
                 false
             }
-            Msg::AdminWsCmdResponse(resp) => {
+            Msg::AdminWs(WsMsg::CmdResponse(resp)) => {
                 match resp {
                     Ok(val) => {
                         console_log!(format!("AdminWsCmdResponse: {:?}", val));
@@ -96,17 +82,17 @@ impl Component for Model {
 
             // AppWs
             // TODO consider consolidating / deduplicating admin and app bits
-            Msg::AppWsConnected(ws) => {
+            Msg::AppWs(WsMsg::Connected(ws)) => {
                 self.app_ws = WsState::Present(ws.clone());
                 console_log!("Holochain app ws connected: {:?}", ws);
                 true
             }
-            Msg::AppWsError(err) => {
+            Msg::AppWs(WsMsg::Error(err)) => {
                 self.app_ws = WsState::Absent(err.clone());
                 console_error!(format!("AppWsError: {}", err));
                 true
             }
-            Msg::AppWsCmd(cmd) => {
+            Msg::AppWs(WsMsg::Cmd(cmd)) => {
                 let ws_clone = self.app_ws.clone();
                 match ws_clone {
                     WsState::Absent(err) => {
@@ -114,13 +100,14 @@ impl Component for Model {
                     }
                     WsState::Present(ws) => {
                         console_log!("AppWsCmd w/ app ws");
-                        ctx.link()
-                            .send_future(async move { Msg::AppWsCmdResponse(ws.call(cmd).await) });
+                        ctx.link().send_future(async move {
+                            Msg::AppWs(WsMsg::CmdResponse(ws.call(cmd).await))
+                        });
                     }
                 };
                 false
             }
-            Msg::AppWsCmdResponse(resp) => {
+            Msg::AppWs(WsMsg::CmdResponse(resp)) => {
                 match resp {
                     Ok(val) => {
                         console_log!(format!("AppWsCmdResponse: {:?}", val));
@@ -140,43 +127,43 @@ impl Component for Model {
             WsState::Present(ws) => format!("typeof: {:?}", ws.js_ws.js_typeof(),),
         };
         let enable_app_handler = |input: String| {
-            Ok(Msg::AdminWsCmd(AdminWsCmd::EnableApp {
+            Ok(Msg::AdminWs(WsMsg::Cmd(AdminWsCmd::EnableApp {
                 installed_app_id: input,
-            }))
+            })))
         };
         let disable_app_handler = |input: String| {
-            Ok(Msg::AdminWsCmd(AdminWsCmd::DisableApp {
+            Ok(Msg::AdminWs(WsMsg::Cmd(AdminWsCmd::DisableApp {
                 installed_app_id: input,
-            }))
+            })))
         };
         let uninstall_app_handler = |input: String| {
-            Ok(Msg::AdminWsCmd(AdminWsCmd::UninstallApp {
+            Ok(Msg::AdminWs(WsMsg::Cmd(AdminWsCmd::UninstallApp {
                 installed_app_id: input,
-            }))
+            })))
         };
         let register_dna_handler = |input: String| {
-            Ok(Msg::AdminWsCmd(AdminWsCmd::RegisterDna {
+            Ok(Msg::AdminWs(WsMsg::Cmd(AdminWsCmd::RegisterDna {
                 path: input,
                 uid: None,
                 properties: None,
-            }))
+            })))
         };
         let attach_app_interface_handler = |input: String| {
             input
                 .parse()
-                .map(|port| Msg::AdminWsCmd(AdminWsCmd::AttachAppInterface { port }))
+                .map(|port| Msg::AdminWs(WsMsg::Cmd(AdminWsCmd::AttachAppInterface { port })))
                 .map_err(|err| format!("attach_app_interface_handler: {}", err))
         };
         let mk_nullary_button = |msg: AdminWsCmd| {
             let msg_ = msg.clone();
             html! {
-                <button onclick={ctx.link().callback(move |_| Msg::AdminWsCmd(msg.clone()))}>{ format!("{:?}", msg_) }</button>
+                <button onclick={ctx.link().callback(move |_| Msg::AdminWs(WsMsg::Cmd(msg.clone())))}>{ format!("{:?}", msg_) }</button>
             }
         };
         let app_info_handler = |input: String| {
-            Ok(Msg::AppWsCmd(AppWsCmd::AppInfo {
+            Ok(Msg::AppWs(WsMsg::Cmd(AppWsCmd::AppInfo {
                 installed_app_id: input,
-            }))
+            })))
         };
         html! {
             <div>
--- crates/widget_helpers/src/lib.rs
+++ crates/widget_helpers/src/lib.rs
@@ -1,1 +1,14 @@
+use wasm_bindgen::prelude::*;
 
+pub enum WsMsg<WS, WSCMD, WSCMDRESP> {
+    Connected(WS),
+    Error(String),
+    Cmd(WSCMD),
+    CmdResponse(Result<WSCMDRESP, JsValue>),
+}
+
+#[derive(Clone, Debug)]
+pub enum WsState<WS> {
+    Present(WS),
+    Absent(String),
+}
