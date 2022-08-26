43efdef7fb9fcba1adabbaf0a3949f247a62a413
Michael Hueschen <m@mhueschen.space>
2022-08-26 16:04:06 -0600

    update widget installed_app_id s

--- crates/memez_ui/main.js
+++ crates/memez_ui/main.js
@@ -6,8 +6,7 @@ async function main() {
   let element = document.getElementById("memez_main");
   let admin_ws_js = await AdminWebsocket.connect("ws://localhost:9000");
   let app_ws_js = await AppWebsocket.connect("ws://localhost:9999");
-  // TODO change this to `{ installed_app_id: 'memez' }`
-  let app_info = await app_ws_js.appInfo({ installed_app_id: 'test-app' });
+  let app_info = await app_ws_js.appInfo({ installed_app_id: 'memez_main_zome' });
   let cell_id_js = app_info.cell_data[0].cell_id;
   run_app(element, admin_ws_js, app_ws_js, cell_id_js);
 }
