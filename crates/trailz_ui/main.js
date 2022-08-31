43dce2ecebd29e76fed69629192fe392973ea3f8
Michael Hueschen <m@mhueschen.space>
2022-08-31 12:50:22 -0600

    trailz: hack to run it solo

--- crates/trailz_ui/main.js
+++ crates/trailz_ui/main.js
@@ -6,7 +6,8 @@ async function main() {
   let element = document.getElementById("trailz_main");
   let admin_ws_js = await AdminWebsocket.connect("ws://localhost:9000");
   let app_ws_js = await AppWebsocket.connect("ws://localhost:9999");
-  let app_info = await app_ws_js.appInfo({ installed_app_id: 'trailz_main_zome' });
+  // TODO change to { installed_app_id: 'trailz_main_zome' }
+  let app_info = await app_ws_js.appInfo({ installed_app_id: 'test-app' });
   let cell_id_js = app_info.cell_data[0].cell_id;
   run_app(element, admin_ws_js, app_ws_js, cell_id_js);
 }
